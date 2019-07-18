3cbb29a46e6c924c380c289c6e10165adc1e8884
katai5plate <katai5plate.sub@gmail.com>
2019-07-19 01:34:19 +0900

    remove dist

--- dist/conv/text.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ini_1 = require("ini");
-var yaml_1 = require("yaml");
-exports.toYaml = function (data) { return yaml_1.stringify(ini_1.parse(data)); };
-exports.toIni = function (data) { return ini_1.stringify(yaml_1.parse(data)); };
