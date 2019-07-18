5314b6f72ae0b5d342bdf2eb912b73f13c991bd2
katai5plate <katai5plate.sub@gmail.com>
2019-07-19 01:47:55 +0900

    改行コードの問題を解消

--- src/conv/bufText.ts
+++ src/conv/bufText.ts
@@ -9,11 +9,18 @@ const chunkString = (str: string, size: number) => {
   ).map(v => v.join(''));
 };
 
-export const bufferText2PureText = (bufferText: string) =>
-  decode(Buffer.from(chunkString(bufferText, 2).map(n => parseInt(n, 16))), code).replace(/\0/g, "")
+const convertCRLF = (s: string, x: string) => s.replace(/\r?\n|\r/g, x)
 
-export const pureTextToBufferText = (p: string) =>
-  [...encode(p.replace(/\r?\n|\r/g, '\r\n'), code)]
+export const bufferText2PureText = (bufferText: string) =>
+  convertCRLF(
+    decode(
+      Buffer.from(chunkString(bufferText, 2).map(n => parseInt(n, 16))), code
+    ).replace(/\0/g, ""), '\n'
+  )
+
+export const pureTextToBufferText = (pureText: string) => {
+  return [...encode(convertCRLF(pureText, '\r\n'), code)]
     .map(n => n.toString(16).padStart(2, '0'))
     .join('')
     .padEnd(4096, '0');
+}
